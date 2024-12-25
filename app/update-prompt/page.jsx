ab28dd50399705771ec643d4503c84417440025b
Teddy <teddytp330@gmail.com>
2024-12-25 20:35:35 +0630

    fix one

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,23 +1,23 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from '@components/Form'
 
 const EditPrompt = () => {
     const router = useRouter()
-    const [ submitting, setSubmitting ] = useState(false)
+    const [submitting, setSubmitting] = useState(false)
     const searchParams = useSearchParams()
-    const { data: session} = useSession()
+    const { data: session } = useSession()
     const promptId = searchParams.get('id')
-    const [ post, setPost ] = useState({
+    const [post, setPost] = useState({
         prompt: '',
         tag: ''
     })
 
-    useEffect(()=>{
-        const getPromptDetai = async() =>{
+    useEffect(() => {
+        const getPromptDetail = async () => {
             const res = await fetch(`/api/prompt/${promptId}`)
             const data = await res.json()
 
@@ -27,40 +27,47 @@ const EditPrompt = () => {
             })
         }
 
-        if( promptId ) getPromptDetai()
+        if (promptId) getPromptDetail()
     }, [promptId])
 
-    const updatePropmt = async( e ) =>{
+    const updatePrompt = async (e) => {
         e.preventDefault()
-        setSubmitting( true )
-        if( !promptId) return alert('prompt id not provided')
+        setSubmitting(true)
+        if (!promptId) return alert('prompt id not provided')
         try {
-            const res = await fetch(`/api/prompt/${promptId}`,{
-                method:'PATCH',
+            const res = await fetch(`/api/prompt/${promptId}`, {
+                method: 'PATCH',
                 body: JSON.stringify({
                     prompt: post.prompt,
                     userId: session?.user.id,
                     tag: post.tag
                 })
             })
-            if( res.ok){
+            if (res.ok) {
                 router.push('/')
             }
         } catch (error) {
             console.log(error)
         } finally {
-            setSubmitting( false )
+            setSubmitting(false)
         }
     }
-  return (
-    <Form
-        type = ' Edit'
-        post = { post }
-        setPost = { setPost }
-        submitting = { submitting }
-        handleSubmit = { updatePropmt }
-    />
-  )
+
+    return (
+        <Form
+            type='Edit'
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
+    )
 }
 
-export default EditPrompt
+const EditPromptPage = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <EditPrompt />
+    </Suspense>
+)
+
+export default EditPromptPage
